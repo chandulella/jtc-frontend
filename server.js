2ae1488b0a7a6adf910aaa4ddbe25aa802d03a7d
chandulella <chandu.sai@venturepact.com>
2019-02-15 14:47:33 +0530

    added few files

--- server.js
+++ server.js
@@ -2,16 +2,10 @@ const express=require('express');
 const path=require('path');
 const app=express();
 
-//=====not for dynamic routes===
-// app.use(express.static(__dirname+'/dist/frntend'));
-// app.get('', function(req, res) {
-//     res.sendFile(path.join(__dirname + '/dist/frntend/index.html'));
-// });
-console.log(__dirname)
-app.use(express.static(__dirname+'/dist/jtc-frontend'));
+
+app.use(express.static(__dirname+'/dist/jtc-frontend', {index: false}));
 app.get('*', (request, response) => {
-    console.log(__dirname,__dirname+'/dist/jtc-frontend',__dirname+'dist/jtc-frontend')
-	response.sendFile(path.join(__dirname, '/dist/jtc-frontend', 'index.html'));
+	response.sendFile(path.join(__dirname, 'dist/jtc-frontend', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080,()=>{
