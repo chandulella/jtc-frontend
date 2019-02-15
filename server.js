21ab7cf6499cbd7e381cb864aa79566d4f12920a
chandulella <chandu.sai@venturepact.com>
2019-02-15 13:52:24 +0530

    changes ins erver.js

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app=express();
 // app.get('', function(req, res) {
 //     res.sendFile(path.join(__dirname + '/dist/frntend/index.html'));
 // });
-
+console.log(__dirname)
 app.use(express.static(__dirname+'/dist/jtc-frontend'));
 app.get('*', (request, response) => {
+    console.log(__dirname,__dirname+'/dist/jtc-frontend',__dirname+'dist/jtc-frontend')
 	response.sendFile(path.join(__dirname, '/dist/jtc-frontend', 'index.html'));
 });
 
